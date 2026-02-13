9a0ac6de60727429d655de397bea16b2c9864d2d
Elena Alexeeva <Elena.1Alexeeva@gmail.com>
2026-02-13 10:05:01 +0000

    Fix layout main background

--- src/app/components/layout-client.tsx
+++ src/app/components/layout-client.tsx
@@ -15,7 +15,7 @@ export function LayoutClient({ children }: { children: React.ReactNode }) {
       <div className="flex min-h-screen flex-col">
         {false && <TopBar />}
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 bg-white">{children}</main>
         <Footer />
       </div>
 
