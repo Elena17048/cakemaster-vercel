b6c584f21c79bc1d800a7f765a8f245363a59b70
elena.1alexeeva <elena.1alexeeva@gmail.com>
2026-03-12 16:42:07 +0000

    debug QR string

--- src/app/courses/payment/page.tsx
+++ src/app/courses/payment/page.tsx
@@ -45,7 +45,7 @@ export default function PaymentPage() {
       if (numericPrice && vs) {
 
         const qrString =
-          `SPD*1.0*ACC:CZ84080000006155124013*AM:${numericPrice.toFixed(2)}*CC:CZK*X-VS:${vs}*MSG:Cukrarske kurzy`;
+  `SPD*1.0*ACC:6155124013/0800*AM:${numericPrice.toFixed(2)}*CC:CZK*X-VS:${vs}*MSG:Cukrarske kurzy`;
 
         console.log("QR STRING:", qrString);
 
